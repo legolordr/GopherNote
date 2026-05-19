a2aa8e80a226b5b5a5fe24ccdc3564fbc33f7a2f
legolordr <Ishmametev_den@mail.ru>
2026-05-19 19:00:32 +0500

    Register NoteService

--- GopherNote/Program.cs
+++ GopherNote/Program.cs
@@ -1,11 +1,15 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using GopherNote;
+using GopherNote.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+//Регистрируем сервисы
+builder.Services.AddScoped<NoteService>();
 
 await builder.Build().RunAsync();
