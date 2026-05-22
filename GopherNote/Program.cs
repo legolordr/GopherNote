379818999ff1faf2e0b294175865daf419fe3e1a
legolordr <Ishmametev_den@mail.ru>
2026-05-22 19:35:43 +0500

    connect Components in _Imports.razor, connect SpeechService

--- GopherNote/Program.cs
+++ GopherNote/Program.cs
@@ -12,5 +12,6 @@ builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.
 //Регистрируем сервисы
 builder.Services.AddScoped<NoteService>();
 builder.Services.AddScoped<QuoteService>(); // сервиc для контекстных цитат
+builder.Services.AddScoped<SpeechService>(); //connect SpeechService
 
 await builder.Build().RunAsync();
